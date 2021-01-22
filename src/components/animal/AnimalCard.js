bed72124543f7936623e04c671201468f78dfb8b
Xine75 <cjdoza@gmail.com>
2021-01-22 13:46:48 -0600

    merge fix

--- src/components/animal/AnimalCard.js
+++ src/components/animal/AnimalCard.js
@@ -1,10 +1,12 @@
 import React from "react"
 import "./Animal.css"
 
-export const AnimalCard = ({animal}) => (
+export const AnimalCard = ({animal, customer, location }) => (
     <section className="animal">
         <h3 className="animal__name">{animal.name}</h3>
         <div className="animal__breed">{animal.breed}</div>
         <address className="location__address">{animal.location.name}</address>
+        <div className="animal__location">{location.name}</div>
+        <div className="animal__customer">{customer.name}</div>
     </section>
 )
